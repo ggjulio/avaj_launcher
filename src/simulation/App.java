35d90ca23f6380954e38e64405d15fc7a1877cd7
Julio Gonzalez <gonzalez.julio8959@gmail.com>
2022-06-15 11:32:28 +0000

    .

--- src/simulation/App.java
+++ src/simulation/App.java
@@ -2,6 +2,7 @@ package simulation;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,14 +27,25 @@ public class App {
 
 				if (nbSimulations <= 0)
 					throw new InvalidSimulationCountException(String.format("Invalid simulations count %d", nbSimulations));
+				int idxLine = 0;
 				while ((line = br.readLine()) != null) {
 					var arr = line.split(" ");
+					if ( arr.length != 5 )
+						throw new Exception(
+							String.format("Too %s arguments provided at line %d.\n"
+										+"The format is: TYPE NAME LONGITUDE LATITUDE HEIGHT",
+										(arr.length < 5 ? "few" : "much"), idxLine));
+					if (Integer.parseInt(arr[2]) < 0 || Integer.parseInt(arr[3]) < 0 || Integer.parseInt(arr[4]) < 0)
+						throw new Exception(String.format("Negative coordinate a line %d. Please put fix it!", idxLine));
+					
 					Flyable flyable = AircraftFactory.newAircraft(
 							arr[0], arr[1],
 							Integer.parseInt(arr[2]),
 							Integer.parseInt(arr[3]),
 							Integer.parseInt(arr[4]));
 					flyables.add(flyable);
+					++idxLine;
+
 				}
 				for (Flyable flyable : flyables)
 					flyable.registerTower(weatherTower);
--- src/weather/Coordinates.java
+++ src/weather/Coordinates.java
@@ -7,14 +7,10 @@ public class Coordinates {
     private int latitude;
     private int height;
 
-    public Coordinates(int longitude, int latitude, int height) throws NegativeCoordinatesException {
+    public Coordinates(int longitude, int latitude, int height) {
         this.longitude = longitude;
         this.latitude = latitude;
         this.height = height;
-        throw new NegativeCoordinatesException(
-            String.format("Coordinates can't be negative: lng=%d, lat=%d, height=%d", 
-                longitude,latitude, height
-        ));
     }
     public int getLongitude() {
         return this.longitude;
